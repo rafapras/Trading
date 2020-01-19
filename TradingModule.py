1c52f3ae09174d05dd2b6001dad5c6ba2f178da7
rafapras <45179674+rafapras@users.noreply.github.com>
2020-01-19 15:46:42 -0300

    refatoracao

--- TradingModule.py
+++ TradingModule.py
@@ -2,69 +2,123 @@ import pandas as pd
 import numpy as np 
 import pickle
 
-inic =''
-with open('C:\\Users\\Rafael\\Trading\\Trading\\ibov.pkl','rb') as file:
+
+
+
+
+class estrategias:
+    decisao_previa =''  
+    preco_inicial=0
+    resultado_previo=0      
+    
+    with open('C:\\Users\\Rafael\\Trading\\Trading\\ibov.pkl','rb') as file:
         data =pickle.load(file)
-data1 =data['Adj Close']
-def estrat(x):
-    global inic
-    if inic!=x:
-        inic = x
-        return x
-    
-    else:
-        return np.nan
+    data1 =data['Adj Close']
+    def estrat(self,x):
+        
+        if self.decisao_previa!=x:
+            self.decisao_previa = x
+            return x
+        
+        else:
+            return np.nan
 
 
-def rollmeanstrat(serie,longmean=100,shortmean=10):
-    
-    serie =pd.DataFrame(serie)
+    def estrat_mmovel(self,serie,longmean=100,shortmean=10):
 
    
+            
+        serie =pd.DataFrame(serie)
+
+        
 
 
 
-    serie['roll']   =  serie.rolling(shortmean,min_periods=5).mean()-serie.rolling(longmean,min_periods=5).mean()
-    
+        serie['roll']   =  serie.rolling(shortmean,min_periods=5).mean()-serie.rolling(longmean,min_periods=5).mean()
+        
 
-    serie['DECISAO']=np.nan
-    
-    serie.loc[serie['roll']>0.0,'DECISAO']='COMPRA'
-    serie.loc[serie['roll']<=0.0,'DECISAO'] ='VENDA'
-    
-    serie['ESTRAT'] =serie['DECISAO'].apply(func=estrat)
-
-    
-    x =returns(serie)
-    
-    return x
-def returns(serie,tipo_oper ='COMPRA'):
+        serie['DECISAO']=np.nan
+        
+        serie.loc[serie['roll']>0.0,'DECISAO']='COMPRA'
+        serie.loc[serie['roll']<=0.0,'DECISAO'] ='VENDA'
+        serie=serie.dropna()
+        serie['ESTRAT'] =serie['DECISAO'].apply(func=self.estrat)
 
     
-    serie=serie.dropna()
+        
+        return serie
+    def returns(self,serie,tipo_oper ='COMPRA'):
 
-    serie['LUCRO']= serie.iloc[:,0].shift(-1)/(serie.iloc[:,0])
+        
+        serie=serie.dropna()
 
-
-    serie= serie.loc[(serie['ESTRAT']==tipo_oper) & (pd.notna(serie['LUCRO']))]
-
-
-
-
-
-    retorno_total=serie['LUCRO'].rolling(len(serie )).apply(np.prod, raw=True).dropna()
-
-    return retorno_total
+        serie['LUCRO']= serie.iloc[:,0].shift(-1)/(serie.iloc[:,0])
 
 
-def worker(params):
+        serie= serie.loc[(serie['ESTRAT']==tipo_oper) & (pd.notna(serie['LUCRO']))]
+
+
+
+
+
+        retorno_total=serie['LUCRO'].rolling(len(serie )).apply(np.prod, raw=True).dropna()
+
+        return retorno_total
+
+    def estrat_retornos(self,x):
     
-    global data1
-    pars = {}
-    pars2 = {}
-    for i in params:
-        z,y=i[0],i[1]
-        test =data1.apply(rollmeanstrat,args=(z,y)) 
-        pars[(z,y)]=test.sum().mean()
-        pars2[(z,y)]=test.sum().median()
-    return pars
+        
+        
+        
+        if x['DECISAO']=='COMPRA' and x['DECISAO']!=self.decisao_previa  :
+            self.preco_inicial=x.iloc[0]
+            self.decisao_previa='COMPRA'
+        elif    x['DECISAO']=='COMPRA' and x['DECISAO']==self.decisao_previa  :
+            if self.resultado_previo!=0:
+                return((x.iloc[0]/self.preco_inicial)*self.resultado_previo)
+            else: 
+                return(x.iloc[0]/self.preco_inicial)
+        elif x['DECISAO']=='VENDA' and self.decisao_previa =='COMPRA':
+            self.decisao_previa='VENDA'
+            if self.resultado_previo!=0:
+                self.resultado_previo=(x.iloc[0]/self.preco_inicial*self.resultado_previo)
+            else :
+                self.resultado_previo = x.iloc[0]/self.preco_inicial
+            
+            return(self.resultado_previo)
+    def estrat_retornos1(self,x):
+    
+        
+        
+        if x['DECISAO']=='COMPRA' and x['DECISAO']!=self.decisao_previa  :
+            self.preco_inicial=x.iloc[0]
+            self.decisao_previa=x['DECISAO']
+        elif    x['DECISAO']=='COMPRA' and x['DECISAO']==self.decisao_previa  :
+            return((x.iloc[0]/self.preco_inicial))
+            
+        elif x['DECISAO']=='VENDA' and self.decisao_previa =='COMPRA':
+            self.decisao_previa='VENDA'
+            self.resultado_previo=(x.iloc[0]/self.preco_inicial)
+                       
+            return(self.resultado_previo)
+    
+
+
+
+
+
+
+
+
+
+    def worker(self,params):
+        
+        global data1
+        pars = {}
+        pars2 = {}
+        for i in  params:
+            z,y=i[0],i[1]
+            test =self.data1.apply(estrat_mmovel,args=(z,y)) 
+            pars[(z,y)]=test.sum().mean()
+            pars2[(z,y)]=test.sum().median()
+        return pars
